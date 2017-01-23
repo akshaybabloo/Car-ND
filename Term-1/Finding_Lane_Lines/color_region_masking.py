f6c5e7c79f7b7bb0bf81b0b938354cb984779a51
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-01-23 14:10:23 +1300

    optimum triangle coordinates added

--- Term-1/Finding_Lane_Lines/color_region_masking.py
+++ Term-1/Finding_Lane_Lines/color_region_masking.py
@@ -21,9 +21,9 @@ blue_threshold = 200
 rgb_threshold = [red_threshold, green_threshold, blue_threshold]
 
 # Init a triangle (this part is variable)
-left_bottom = [0, 539]
-right_bottom = [900, 300]
-apex = [400, 0]
+left_bottom = [100, 525]  # [0, 539]
+right_bottom = [850, 550]  # [900, 539]
+apex = [460, 310]  # [475, 320]
 
 # See https://docs.scipy.org/doc/numpy/reference/generated/numpy.polyfit.html for more info
 fit_left = np.polyfit((left_bottom[0], apex[0]), (left_bottom[1], apex[1]), 1)
@@ -37,14 +37,22 @@ xx, yy = np.meshgrid(np.arange(0, xsize), np.arange(0, ysize))
 region_thresholds = (yy > (xx*fit_left[0] + fit_left[1])) & (yy > (xx*fit_right[0] + fit_right[1])) & (yy < (xx*fit_bottom[0] + fit_bottom[1]))
 
 # Mask color selection
-color_select[color_thresholds] = [0, 0, 0]
+color_select[color_thresholds | ~region_thresholds] = [0, 0, 0]
 # Find where image is both colored right and in the region
 line_image[~color_thresholds & region_thresholds] = [255, 0, 0]
 
 # Show figures
 f = plt.figure()
-f.add_subplot(1, 2, 1)
-plt.imshow(color_select)
-f.add_subplot(1, 2, 2)
-plt.imshow(line_image)
+x = [left_bottom[0], right_bottom[0], apex[0], left_bottom[0]]
+y = [left_bottom[1], right_bottom[1], apex[1], left_bottom[1]]
+
+f.add_subplot(1, 3, 1)
+plt.plot(x, y, 'b--', lw=4)
+plt.imshow(image)  # Image superimposed with plotted lines
+
+f.add_subplot(1, 3, 2)
+plt.imshow(color_select)  # bright color line with triangle
+
+f.add_subplot(1, 3, 3)
+plt.imshow(line_image)  # Masked image
 plt.show()
