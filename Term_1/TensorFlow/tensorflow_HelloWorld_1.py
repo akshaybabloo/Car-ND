390cb484e418ea82ac33113ff363e01d5f8c5ebc
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-02-10 16:45:40 +1300

    changed to function

--- Term_1/TensorFlow/tensorflow_HelloWorld_1.py
+++ Term_1/TensorFlow/tensorflow_HelloWorld_1.py
@@ -1,9 +1,17 @@
 import tensorflow as tf
 
-# Create TensorFlow object called tensor
-hello_constant = tf.constant('Hello World!')
 
-with tf.Session() as sess:
-    # Run the tf.constant operation in the session
-    output = sess.run(hello_constant)
-    print(output)
+def run():
+    """
+    Printing `Hello World` using Tensor `constant`.
+    """
+    # Create TensorFlow object called tensor
+    hello_constant = tf.constant('Hello World!')
+
+    with tf.Session() as sess:
+        # Run the tf.constant operation in the session
+        output = sess.run(hello_constant)
+        print(output)
+
+if __name__ == '__main__':
+    run()
