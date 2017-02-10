a648b77d9a829d44a8f7a7b4f978b031721886fe
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-02-10 18:42:44 +1300

    tensor_softmax  added

--- Term_1/TensorFlow/softmax_7.py
+++ Term_1/TensorFlow/softmax_7.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 
 def softmax(x):
@@ -19,8 +20,34 @@ def softmax(x):
     """
     return np.divide(np.exp(x), np.sum(np.exp(x), axis=0))
 
+
+def tensor_softmax(logit_data):
+    """
+    Finding softmax using TensorFlow
+
+    Parameters
+    ----------
+    logit_data
+
+    Returns
+    -------
+
+    TensorFlow Softmax: ndarray
+
+    """
+    output = None
+    logits = tf.placeholder(tf.float32)
+
+    softmax = tf.nn.softmax(logits)
+
+    with tf.Session() as sess:
+        output = sess.run(softmax, feed_dict={logits: logit_data})
+
+    return output
+
 if __name__ == '__main__':
 
     logits = [3.0, 1.0, 0.2]
 
     print(softmax(logits))
+    print(tensor_softmax(logits))
