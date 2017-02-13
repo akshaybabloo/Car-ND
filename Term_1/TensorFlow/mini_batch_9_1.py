648f37541474f8bd71f077e8b86096db4cb8d461
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-02-13 16:36:15 +1300

    docs added

--- Term_1/TensorFlow/mini_batch_9_1.py
+++ Term_1/TensorFlow/mini_batch_9_1.py
@@ -1,28 +1,19 @@
 import pprint
 
-# 4 Samples of features
-example_features = [
-    ['F11', 'F12', 'F13', 'F14'],
-    ['F21', 'F22', 'F23', 'F24'],
-    ['F31', 'F32', 'F33', 'F34'],
-    ['F41', 'F42', 'F43', 'F44']]
-# 4 Samples of labels
-example_labels = [
-    ['L11', 'L12'],
-    ['L21', 'L22'],
-    ['L31', 'L32'],
-    ['L41', 'L42']]
-
-# PPrint prints data structures like 2d arrays, so they are easier to read
-
 
 def batches(batch_size, features, labels):
     """
     Create batches of features and labels
-    :param batch_size: The batch size
-    :param features: List of features
-    :param labels: List of labels
-    :return: Batches of (Features, Labels)
+
+    Parameters
+    ----------
+    batch_size: The batch size
+    features: List of features
+    labels: List of labels
+
+    Returns
+    -------
+        Batches of (Features, Labels) : ndarray
     """
     assert len(features) == len(labels)
     outout_batches = []
@@ -35,6 +26,26 @@ def batches(batch_size, features, labels):
 
     return outout_batches
 
-if __name__ == '__main__':
+
+def run():
+    """
+    Pretty prints the list into batches, for a double list.
+    """
+
+    # 4 Samples of features
+    example_features = [
+        ['F11', 'F12', 'F13', 'F14'],
+        ['F21', 'F22', 'F23', 'F24'],
+        ['F31', 'F32', 'F33', 'F34'],
+        ['F41', 'F42', 'F43', 'F44']]
+    # 4 Samples of labels
+    example_labels = [
+        ['L11', 'L12'],
+        ['L21', 'L22'],
+        ['L31', 'L32'],
+        ['L41', 'L42']]
     pp = pprint.PrettyPrinter(depth=10, width=50)
     pp.pprint(batches(3, example_features, example_labels))
+
+if __name__ == '__main__':
+    run()
