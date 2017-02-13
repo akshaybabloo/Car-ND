5fd31ffd4885beebed11326c18ed993f9d83f613
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-02-13 18:19:50 +1300

    updated

--- Term_1/TensorFlow/LeNet_Lab/main.py
+++ Term_1/TensorFlow/LeNet_Lab/main.py
@@ -29,12 +29,20 @@ X_test = np.pad(X_test, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
 
 print("Updated Image Shape: {}".format(X_train[0].shape))
 
-index = random.randint(0, len(X_train))
-image = X_train[index].squeeze()
 
-plt.figure(figsize=(1, 1))
-plt.imshow(image, cmap="gray")
-print(y_train[index])
+def plot():
+    """
+    Plots an MNIST image.
+
+    """
+    index = random.randint(0, len(X_train))
+    image = X_train[index].squeeze()
+
+    plt.figure(figsize=(1, 1))
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    print(y_train[index])
+
 
 X_train, y_train = shuffle(X_train, y_train)
 
@@ -96,6 +104,17 @@ def LeNet(x):
     return logits
 
 
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
 x = tf.placeholder(tf.float32, (None, 32, 32, 1))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, 10)
@@ -111,18 +130,6 @@ training_operation = optimizer.minimize(loss_operation)
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 saver = tf.train.Saver()
-
-
-def evaluate(X_data, y_data):
-    num_examples = len(X_data)
-    total_accuracy = 0
-    sess = tf.get_default_session()
-    for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
-        total_accuracy += (accuracy * len(batch_x))
-    return total_accuracy / num_examples
-
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
@@ -150,3 +157,7 @@ with tf.Session() as sess:
 
     test_accuracy = evaluate(X_test, y_test)
     print("Test Accuracy = {:.3f}".format(test_accuracy))
+
+
+if __name__ == '__main__':
+    plot()
