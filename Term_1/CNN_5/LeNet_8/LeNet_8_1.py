fe542205b681e88c7c610b079b8ccd54f15aa546
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-06 18:24:13 +1300

    unwanted code removed

--- Term_1/CNN_5/LeNet_8/LeNet_8_1.py
+++ Term_1/CNN_5/LeNet_8/LeNet_8_1.py
@@ -6,35 +6,6 @@ from sklearn.utils import shuffle
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 
-# mnist = input_data.read_data_sets("./MNIST_data/", reshape=False)
-# X_train, y_train = mnist.train.images, mnist.train.labels
-# X_validation, y_validation = mnist.validation.images, mnist.validation.labels
-# X_test, y_test = mnist.test.images, mnist.test.labels
-#
-# assert (len(X_train) == len(y_train))
-# assert (len(X_validation) == len(y_validation))
-# assert (len(X_test) == len(y_test))
-#
-# print()
-# print("Image Shape: {}".format(X_train[0].shape))
-# print()
-# print("Training Set:   {} samples".format(len(X_train)))
-# print("Validation Set: {} samples".format(len(X_validation)))
-# print("Test Set:       {} samples".format(len(X_test)))
-#
-# # Pad images with 0s
-# X_train = np.pad(X_train, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
-# X_validation = np.pad(X_validation, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
-# X_test = np.pad(X_test, ((0, 0), (2, 2), (2, 2), (0, 0)), 'constant')
-#
-# print("Updated Image Shape: {}".format(X_train[0].shape))
-#
-# # Shuffle training data.
-# X_train, y_train = shuffle(X_train, y_train)
-#
-# EPOCHS = 10
-# BATCH_SIZE = 128
-
 
 def LeNet(x):
     """
