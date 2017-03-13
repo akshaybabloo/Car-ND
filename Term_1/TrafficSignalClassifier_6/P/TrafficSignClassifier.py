81d22656633dfd3a49852bddf2b5b992cc2361ae
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-13 14:51:08 +1300

    paramater labels added

--- Term_1/TrafficSignalClassifier_6/P/TrafficSignClassifier.py
+++ Term_1/TrafficSignalClassifier_6/P/TrafficSignClassifier.py
@@ -144,7 +144,7 @@ one_hot_y = tf.one_hot(y, 43)
 RATE = 0.001
 
 logits = LeNet(x)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=RATE)
 training_operation = optimizer.minimize(loss_operation)
