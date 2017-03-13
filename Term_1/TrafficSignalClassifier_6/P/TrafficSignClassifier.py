3d3c45631f3cad122e8774e97c7ef069aa905cd7
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-03-13 16:09:00 +1300

    PEP8

--- Term_1/TrafficSignalClassifier_6/P/TrafficSignClassifier.py
+++ Term_1/TrafficSignalClassifier_6/P/TrafficSignClassifier.py
@@ -85,52 +85,53 @@ def LeNet(x):
     tensor
     """
     # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
-    mu = 0
-    sigma = 0.1
+    MU = 0
+    SIGMA = 0.1
+    PADDING = 'VALID'
 
-    # SOLUTION: Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
-    conv1_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean=mu, stddev=sigma))
+    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    conv1_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean=MU, stddev=SIGMA))
     conv1_b = tf.Variable(tf.zeros(6))
-    conv1 = tf.nn.conv2d(x, conv1_w, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+    conv1 = tf.nn.conv2d(x, conv1_w, strides=[1, 1, 1, 1], padding=PADDING) + conv1_b
 
-    # SOLUTION: Activation.
+    # Activation.
     conv1 = tf.nn.relu(conv1)
 
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding=PADDING)
 
-    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
-    conv2_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
+    # Layer 2: Convolutional. Output = 10x10x16.
+    conv2_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=MU, stddev=SIGMA))
     conv2_b = tf.Variable(tf.zeros(16))
-    conv2 = tf.nn.conv2d(conv1, conv2_w, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    conv2 = tf.nn.conv2d(conv1, conv2_w, strides=[1, 1, 1, 1], padding=PADDING) + conv2_b
 
-    # SOLUTION: Activation.
+    # Activation.
     conv2 = tf.nn.relu(conv2)
 
-    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
+    # Flatten. Input = 5x5x16. Output = 400.
     fc0 = flatten(conv2)
 
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_w = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_w = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=MU, stddev=SIGMA))
     fc1_b = tf.Variable(tf.zeros(120))
     fc1 = tf.matmul(fc0, fc1_w) + fc1_b
 
-    # SOLUTION: Activation.
+    # Activation.
     fc1 = tf.nn.relu(fc1)
 
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_w = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_w = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=MU, stddev=SIGMA))
     fc2_b = tf.Variable(tf.zeros(84))
     fc2 = tf.matmul(fc1, fc2_w) + fc2_b
 
-    # SOLUTION: Activation.
+    # Activation.
     fc2 = tf.nn.relu(fc2)
 
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
-    fc3_w = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=mu, stddev=sigma))
+    # Layer 5: Fully Connected. Input = 84. Output = 43.
+    fc3_w = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=MU, stddev=SIGMA))
     fc3_b = tf.Variable(tf.zeros(43))
     _logits = tf.matmul(fc2, fc3_w) + fc3_b
 
@@ -141,7 +142,7 @@ x = tf.placeholder(tf.float32, (None, 32, 32, 3))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, 43)
 
-RATE = 0.001
+RATE = 0.01
 
 logits = LeNet(x)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot_y)
@@ -218,7 +219,7 @@ def run_testing():
         print("Test Accuracy = {:.3f}".format(test_accuracy))
 
 
-# if __name__ == '__main__':
-    # run_training()
-    # run_testing()
+if __name__ == '__main__':
+    run_training()
+    run_testing()
     # show_image()
