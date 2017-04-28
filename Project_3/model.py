0a9730a6269ae0606cb15886ea1dec313c75fb9e
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-04-28 18:01:18 +1200

    read pickled data and model removed

--- Project_3/model.py
+++ Project_3/model.py
@@ -1,53 +1,16 @@
-import csv
-import os
-
-import cv2
-import numpy as np
+import pickle
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.models import Sequential
 
-full_path_csv = os.path.abspath('data' + os.sep + 'driving_log.csv')
-full_path_img = os.path.abspath('data' + os.sep + 'IMG') + os.sep
+"""
+2. Creating the model
+"""
 
-# Reading the contents in the CSV file
-lines = []
-with open(full_path_csv) as csv_file:
-    reader = csv.reader(csv_file)
-    for line in reader:
-        lines.append(line)
+# Read the pickled data.
+with open('data.p', mode='rb') as f:
+    data = pickle.load(f)
 
-# Reading the images (center, left, right) and its measurements
-images = []
-measurements = []
-for line in lines:
-    for camera in range(3):
-        source_path = line[camera]
-        file_name = source_path.split('/')[-1]
-        current_path = full_path_img + file_name
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3])
-        measurements.append(measurement)
+x_train, y_train = data['x_train'], data['y_train']
 
-# Augmenting the image data and its measurement. Flipping them vertically.
-augmented_images = []
-augmented_measurements = []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))  # Flipping them vertically
-    augmented_measurements.append(measurement * -1.0)
+# Create the model.
 
-x_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
-
-model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))  # normalise data
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Flatten())
-model.add(Dense(1))
-
-model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-
-model.save('model.h5')
