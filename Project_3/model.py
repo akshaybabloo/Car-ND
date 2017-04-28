0c7981aae7f229ac06b50fbe7ce87530de03d535
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-04-28 18:34:47 +1200

    model added

--- Project_3/model.py
+++ Project_3/model.py
@@ -1,5 +1,6 @@
 import pickle
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, ELU, Convolution2D, Dropout
+from keras.optimizers import Adam
 from keras.models import Sequential
 
 """
@@ -13,4 +14,52 @@ with open('data.p', mode='rb') as f:
 x_train, y_train = data['x_train'], data['y_train']
 
 # Create the model.
+model = Sequential()
 
+# normalise data
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+# Conv layer 1
+model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
+model.add(ELU())
+
+# Conv layer 2
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
+model.add(ELU())
+
+# Conv layer 3
+model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode="same"))
+
+model.add(Flatten())
+model.add(Dropout(.2))
+model.add(ELU())
+
+# Fully connected layer 1
+model.add(Dense(512))
+model.add(Dropout(.5))
+model.add(ELU())
+
+# Fully connected layer 2
+model.add(Dense(50))
+model.add(ELU())
+
+model.add(Dense(1))
+
+adam = Adam(lr=0.0001)
+
+model.compile(optimizer=adam, loss="mse", metrics=['accuracy'])
+
+print("Model summary:\n", model.summary())
+
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=7)
+
+# Saving model to json file
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+
+# Saving weights
+model.save_weights("model.h5")
+print("Saved model to disk")
