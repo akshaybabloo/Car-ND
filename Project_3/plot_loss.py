aeedc7be7368f0d35afb51512480287d8cfb9b9f
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-01 20:41:24 +1200

    loss and average loss added

--- Project_3/plot_loss.py
+++ Project_3/plot_loss.py
@@ -7,7 +7,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 loss_file = os.path.abspath('model_loss.csv')
-
 loss_data = pd.read_csv(loss_file, sep=',', header=None)
-loss_data.plot(kind='line')
+
+ax = loss_data.plot(kind='line', colormap='jet', markersize=10, title="Loss of 100 Epochs for training the data")
+ax.legend(["Loss"])
+ax.set_xlabel("Epochs")
+ax.set_ylabel("Loss Value")
+
+avg_loss_data = loss_data.groupby(loss_data.index // 1025).mean()
+ax2 = avg_loss_data.plot(kind='line', colormap='jet', markersize=10,
+                         title="Average Loss 100 Epochs for training the data")
+ax2.legend(["Loss"])
+ax2.set_xlabel("Epochs")
+ax2.set_ylabel("Loss Value")
+
 plt.show()
