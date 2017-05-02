f6c3c85c41ebc61c4ec763f3c57691540b06aa1c
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-02 19:46:33 +1200

    comments added

--- Term_1/advanced_lane_finding_10/sobel_10_4.py
+++ Term_1/advanced_lane_finding_10/sobel_10_4.py
@@ -1,23 +1,38 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
-def run():
-    img = cv2.imread('curved-lane.jpg')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0)  # (1,0) denotes the x direction
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1)  # (0,1) denotes the y direction
-    abs_sobelx = np.absolute(sobelx)
-    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))  # convert absolute value to 8-bit
+def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # Apply x or y gradient with the OpenCV Sobel() function
+    # and take the absolute value
+    abs_sobel = None
+    if orient == 'x':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 1, 0))
+    if orient == 'y':
+        abs_sobel = np.absolute(cv2.Sobel(gray, cv2.CV_64F, 0, 1))
+    # Rescale back to 8 bit integer
+    scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
+    # Create a copy and apply the threshold
+    binary_output = np.zeros_like(scaled_sobel)
+    # Here I'm using inclusive (>=, <=) thresholds, but exclusive is ok too
+    binary_output[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
 
-    thresh_min = 20
-    thresh_max = 100
+    # Return the result
+    return binary_output
 
-    sxbinary = np.zeros_like(scaled_sobel)
-    sxbinary[(scaled_sobel >= thresh_min) & (scaled_sobel <= thresh_max)] = 1
-    plt.imshow(sxbinary, cmap='gray')
-    plt.show()
 
-if __name__ == '__main__':
-    run()
+image = mpimg.imread('curved-lane.jpg')
+grad_binary = abs_sobel_thresh(image, orient='y', thresh_min=20, thresh_max=100)
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=50)
+ax2.imshow(grad_binary, cmap='gray')
+ax2.set_title('Thresholded Gradient', fontsize=50)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
