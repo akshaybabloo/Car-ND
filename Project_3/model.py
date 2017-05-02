e83d792848406b9ba773c36f14744eb2ceb5b201
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-02 12:14:50 +1200

    model updated for (64,64,3) input

--- Project_3/model.py
+++ Project_3/model.py
@@ -2,48 +2,31 @@ import os
 import pickle
 
 import numpy as np
-from keras.callbacks import ModelCheckpoint, Callback
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, ELU, Conv2D, Dropout
 from keras.models import Sequential
 from keras.optimizers import Adam
+
+import preprocessor_modeler
 
 """
 2. Creating the model
 """
 
 
-class LossHistory(Callback):
-    """
-    Getting the loss for the epochs.
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.losses = []
-
-    def on_train_begin(self, logs=None):
-        if logs is None:
-            logs = {}
-
-    def on_batch_end(self, batch, logs=None):
-        if logs is None:
-            logs = {}
-        self.losses.append(logs.get('loss'))
-
-
 # Read the pickled data.
-with open('data.p', mode='rb') as f:
-    data = pickle.load(f)
-
-x_train, y_train = data['x_train'], data['y_train']
+# with open('data.p', mode='rb') as f:
+#     data = pickle.load(f)
+#
+# x_train, y_train = data['x_train'], data['y_train']
 
 # Create the model.
 model = Sequential()
 
 # normalise data
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(64, 64, 3)))
 
-model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+# model.add(Cropping2D(cropping=((70, 25), (0, 0))))
 
 # Conv layer 1
 model.add(Conv2D(16, (8, 8), strides=(4, 4), padding="same"))
@@ -82,10 +65,20 @@ print("Model summary:\n", model.summary())
 if not os.path.isdir('temp'):
     os.mkdir('temp')
 
-check_point = ModelCheckpoint(filepath="temp/weights.{epoch:02d}-{val_loss:.2f}.hdf5", verbose=1, save_best_only=False)
+# check_point = ModelCheckpoint(filepath="temp/weights.{epoch:02d}-{val_loss:.2f}.hdf5", verbose=1, save_best_only=False)
 
-history = LossHistory()
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=100, callbacks=[history, check_point])
+history_loss = preprocessor_modeler.LossHistory()
+# model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=100, callbacks=[history, check_point])
+
+train_gen = preprocessor_modeler.generate_next_batch()
+validation_gen = preprocessor_modeler.generate_next_batch()
+
+history = model.fit_generator(train_gen,
+                              steps_per_epoch =20032,
+                              epochs=8,
+                              validation_data=validation_gen,
+                              validation_steps=6400,
+                              verbose=1, callbacks=[history_loss])
 
 # Save loss to file
 loss_array = np.asarray(history.losses)
