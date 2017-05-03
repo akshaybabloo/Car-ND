ee66a9bec187908504ad3015fff5abc52ff93c60
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2017-05-04 11:44:33 +1200

    load_model and h5py added unwanted comments removed

--- Project_3/drive.py
+++ Project_3/drive.py
@@ -4,6 +4,7 @@ import json
 import os
 import shutil
 from io import BytesIO
+import h5py
 
 import eventlet.wsgi
 import numpy as np
@@ -11,6 +12,7 @@ import socketio
 from PIL import Image
 from flask import Flask
 from keras.models import model_from_json
+from keras.models import load_model
 from keras import __version__ as keras_version
 
 import preprocessor_modeler
@@ -74,22 +76,6 @@ def send_control(steering_angle, throttle):
 
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='Remote Driving')
-    # parser.add_argument('model', type=str,
-    #                     help='Path to model definition json. Model weights should be on the same path.')
-    # args = parser.parse_args()
-    # with open(args.model, 'r') as jfile:
-    #     model = model_from_json(json.load(jfile))
-    #
-    # model.compile("adam", "mse")
-    # weights_file = args.model.replace('json', 'h5')
-    # model.load_weights(weights_file)
-    #
-    # # wrap Flask application with engineio's middleware
-    # app = socketio.Middleware(sio, app)
-    #
-    # # deploy as an eventlet WSGI server
-    # eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
     parser = argparse.ArgumentParser(description='Remote Driving')
     parser.add_argument(
         'model',
