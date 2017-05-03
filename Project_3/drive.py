25c7d850a9d655b4553b10bdb49143027e9858ec
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-04 11:30:08 +1200

    updated

--- Project_3/drive.py
+++ Project_3/drive.py
@@ -6,14 +6,12 @@ from io import BytesIO
 import eventlet.wsgi
 import numpy as np
 import socketio
-import tensorflow as tf
 from PIL import Image
 from flask import Flask
 from keras.models import model_from_json
 
 import preprocessor_modeler
 
-tf.python.control_flow_ops = tf
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -21,60 +19,42 @@ model = None
 prev_image_array = None
 
 
-class SimplePIController:
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.set_point = 0.
-        self.error = 0.
-        self.integral = 0.
-
-    def set_desired(self, desired):
-        self.set_point = desired
-
-    def update(self, measurement):
-        # proportional error
-        self.error = self.set_point - measurement
-
-        # integral error
-        self.integral += self.error
-
-        return self.Kp * self.error + self.Ki * self.integral
-
-
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
-controller.set_desired(set_speed)
+def crop(image, top_cropping_percent):
+    assert 0 <= top_cropping_percent < 1.0, 'top_cropping_percent should be between zero and one'
+    percent = int(np.ceil(image.shape[0] * top_cropping_percent))
+    return image[percent:, :, :]
 
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    if data:
-        # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
-        # The current speed of the car
-        speed = data["speed"]
-        # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+    # The current steering angle of the car
+    steering_angle = data["steering_angle"]
 
-        throttle = controller.update(float(speed))
+    # The current throttle of the car
+    throttle = data["throttle"]
 
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
+    # The current speed of the car
+    speed = data["speed"]
 
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
-    else:
-        # NOTE: DON'T EDIT THIS.
-        sio.emit('manual', data={}, skip_sid=True)
+    # The current image from the center camera of the car
+    imgString = data["image"]
+    image = Image.open(BytesIO(base64.b64decode(imgString)))
+    image_array = np.asarray(image)
+
+    image_array = preprocessor_modeler.crop(image_array, 0.35, 0.1)
+    image_array = preprocessor_modeler.resize(image_array, new_dim=(64, 64))
+
+    transformed_image_array = image_array[None, :, :, :]
+
+    # This model currently assumes that the features of the model are just the images. Feel free to change this.
+
+    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+    # The driving model currently just outputs a constant throttle. Feel free to edit this.
+    throttle = 0.3
+
+    print('{:.5f}, {:.1f}'.format(steering_angle, throttle))
+
+    send_control(steering_angle, throttle)
 
 
 @sio.on('connect')
