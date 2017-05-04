cb1cd243d9f418c9c323f2cac0b9a91f3282cae2
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-04 12:27:44 +1200

    speed controller and frames saver added

--- Project_3/drive.py
+++ Project_3/drive.py
@@ -14,9 +14,9 @@ from flask import Flask
 from keras.models import model_from_json
 from keras.models import load_model
 from keras import __version__ as keras_version
+import datetime
 
 import preprocessor_modeler
-
 
 sio = socketio.Server()
 app = Flask(__name__)
@@ -30,36 +30,72 @@ def crop(image, top_cropping_percent):
     return image[percent:, :, :]
 
 
+class SimplePIController:
+    def __init__(self, Kp, Ki):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.set_point = 0.
+        self.error = 0.
+        self.integral = 0.
+
+    def set_desired(self, desired):
+        self.set_point = desired
+
+    def update(self, measurement):
+        # proportional error
+        self.error = self.set_point - measurement
+
+        # integral error
+        self.integral += self.error
+
+        return self.Kp * self.error + self.Ki * self.integral
+
+
+controller = SimplePIController(0.1, 0.002)
+set_speed = 9
+controller.set_desired(set_speed)
+
+
 @sio.on('telemetry')
 def telemetry(sid, data):
-    # The current steering angle of the car
-    steering_angle = data["steering_angle"]
+    if data:
+        # The current steering angle of the car
+        steering_angle = data["steering_angle"]
 
-    # The current throttle of the car
-    throttle = data["throttle"]
+        # The current throttle of the car
+        throttle = data["throttle"]
 
-    # The current speed of the car
-    speed = data["speed"]
+        # The current speed of the car
+        speed = data["speed"]
 
-    # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
+        # The current image from the center camera of the car
+        imgString = data["image"]
+        image = Image.open(BytesIO(base64.b64decode(imgString)))
+        image_array = np.asarray(image)
 
-    image_array = preprocessor_modeler.crop(image_array, 0.35, 0.1)
-    image_array = preprocessor_modeler.resize(image_array, new_dim=(64, 64))
+        image_array = preprocessor_modeler.crop(image_array, 0.35, 0.1)
+        image_array = preprocessor_modeler.resize(image_array, new_dim=(64, 64))
 
-    transformed_image_array = image_array[None, :, :, :]
+        transformed_image_array = image_array[None, :, :, :]
 
-    # This model currently assumes that the features of the model are just the images. Feel free to change this.
+        # This model currently assumes that the features of the model are just the images. Feel free to change this.
 
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.3
+        steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+        # The driving model currently just outputs a constant throttle. Feel free to edit this.
+        throttle = controller.update(float(speed))
 
-    print('{:.5f}, {:.1f}'.format(steering_angle, throttle))
+        print('{:.5f}, {:.1f}'.format(steering_angle, throttle))
 
-    send_control(steering_angle, throttle)
+        send_control(steering_angle, throttle)
+
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+
+    else:
+        sio.emit('manual', data={}, skip_sid=True)
 
 
 @sio.on('connect')
