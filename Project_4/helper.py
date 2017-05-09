40d57996454fa34f9be6dfb4690c6c193006d1bf
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-09 15:43:13 +1200

    helper init

---
+++ Project_4/helper.py
@@ -0,0 +1,24 @@
+"""
+Cluster of helper functions.
+"""
+
+
+def read_frames():
+    """
+    Read the frames of the video.
+    Returns
+    -------
+
+    """
+    pass
+
+
+def write_frames():
+    """
+    Write frames (images) to a video files.
+    
+    Returns
+    -------
+
+    """
+    pass
