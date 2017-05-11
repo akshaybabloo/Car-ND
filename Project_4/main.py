42110982d93aa9465c8aed0fc747452b13d20b20
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-05-11 22:21:20 +1200

    comments added and os indipendent

--- Project_4/main.py
+++ Project_4/main.py
@@ -2,16 +2,18 @@
 Script to run all the methods & function and view them appropriately.
 """
 
+import os
+import pickle
+from glob import glob
+
+import numpy as np
+from PIL import Image
+from moviepy.editor import ImageSequenceClip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from scipy.misc import imread
+
 import helper
 import processor
-import numpy as np
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from moviepy.editor import ImageSequenceClip
-from scipy.misc import imread
-import matplotlib.pyplot as plt
-from PIL import Image
-from glob import glob
-import pickle
 
 HIST_STEPS = 10
 OFFSET = 250
@@ -28,7 +30,6 @@ DST = np.float32([
     (SRC[-1][0] - OFFSET, 0),
     (SRC[-1][0] - OFFSET, 720)])
 
-
 VIDEO_LOCATION = 'video/project_video.mp4'
 
 
@@ -40,14 +41,18 @@ def to_image_sequence():
 
     for idx, frame in enumerate(video_in.iter_frames()):
         im = Image.fromarray(frame)
-        im.save('video/seq/img_{}.jpeg'.format(idx))
+        im.save('video' + os.sep + 'seq' + os.sep + 'img_{}.jpeg'.format(idx))
 
 
 def run():
     """
-    
+    Runs the flow.
     """
-    
+
+    if not os.path.isdir('video' + os.sep + 'seq'):
+        os.mkdir('video' + os.sep + 'seq')
+        to_image_sequence()
+
     video_in = VideoFileClip(VIDEO_LOCATION)
     video_size = tuple(video_in.size)
 
@@ -62,26 +67,27 @@ def run():
     for i in range(len(content)):
         # images.append(imread('../video/seq/img_%s.jpeg' % i))
         images.append(imread('video/seq/img_%s.jpeg' % i))
-        print(content_len-i)
+        print(content_len - i)
 
     rows = len(images)
-    add_to_me = []
+    processed_images = []
     for row in range(rows):
         img = images[row]
 
-        ld = processor.DetectLanes(SRC, DST, number_frame=FRAME_MEMORY, camera_calibration=cam_calibrator, transform_offset=OFFSET)
+        ld = processor.DetectLanes(SRC, DST, number_frame=FRAME_MEMORY, camera_calibration=cam_calibrator,
+                                   transform_offset=OFFSET)
         img = ld.generate_frame(img)
 
-        add_to_me.append(img)
+        processed_images.append(img)
 
         # Write as image
         im = Image.fromarray(img)
         im.save('video/seq_new/img_{}.jpeg'.format(row))
-        print(rows-row)
+        print(rows - row)
 
     # Create a backup.
     with open('data.p', 'w') as p:
-        pickle.dump({'images': add_to_me}, p, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump({'images': processed_images}, p, protocol=pickle.HIGHEST_PROTOCOL)
 
     # Read the contents of processed image and make a video of it.
     new_content = glob('video/seq_new/img_*.jpeg')
@@ -91,19 +97,12 @@ def run():
     for i in range(len(new_content)):
         # images.append(imread('../video/seq/img_%s.jpeg' % i))
         images_new.append(imread('video/seq_new/img_%s.jpeg' % i))
-        print(new_content_len-i)
+        print(new_content_len - i)
 
+    # Write sequence of images to file.
     new_clip = ImageSequenceClip(images_new, fps=video_in.fps)
     new_clip.write_videofile('processed_video.mp4')
 
 
 if __name__ == '__main__':
     run()
-
-
-    # content_new = glob('video/seq_new/*.jpeg')
-    # # print(content_new[0])
-    # sorted(content_new, key=int)
-    # print(content_new)
-
-    # new_clip = ImageSequenceClip()
